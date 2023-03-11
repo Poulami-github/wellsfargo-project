03507ca9d7353550e9704796db512ea4f73814b3
Poulami-github <56485191+Poulami-github@users.noreply.github.com>
2023-03-11 13:11:02 +0530

    Update FileUpload.jsx

--- src/FileUpload/FileUpload.jsx
+++ src/FileUpload/FileUpload.jsx
@@ -45,7 +45,7 @@ const FileUpload = ({ files, setFiles, removeFile }) => {
                 </div>
 
                 <p className="main">Supported files</p>
-                <p className="info">PDF, JPG, PNG, CSV</p>
+                <p className="info">CSV, XLSX</p>
 
             </div>
         </>
