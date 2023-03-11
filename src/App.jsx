47f47e667a59c8d98b825869a41247f1bbf0bfa5
Poulami-github <56485191+Poulami-github@users.noreply.github.com>
2023-03-11 13:00:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.scss';
 import FileUpload from './FileUpload/FileUpload';
-import FileList from './FileList/FileList';
 
 function App() {
   const [files, setFiles] = useState([])
@@ -15,7 +14,6 @@ function App() {
       <div className="title">Upload file</div>
       <FileUpload files={files} setFiles={setFiles}
         removeFile={removeFile} />
-      <FileList files={files} removeFile={removeFile} />
     </div>
   );
 }
